9a44ee5e7ee7c84809f669e7ed529daabc299194
lindsayMecher <lindsaymecher@gmail.com>
2021-01-12 12:52:01 -0600

    commit tree 92b8c30e565d4e8ea55e47aabcd1c1bcc90c6f35

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,4 +1,5 @@
 def count_strings(array)
+  binding.pry
   array.count do |element|
     element.is_a? String
   end 
@@ -9,3 +10,27 @@ def count_empty_strings(array)
     element == ""
   end 
 end
+
+# require "pry"
+
+# def count_strings(array)
+#   # Return the total number of strings in the provided array using the count enumerable
+#   total = 0
+#   array.count do |item|
+#     if item.class == String
+#       total += 1
+#     end 
+#   end 
+#   total
+# end
+
+# def count_empty_strings(array)
+#   # Return the total number of EMPTY strings in the provided array using the count enumerable
+#   total = 0
+#   array.count do |item|
+#     if item == ""
+#       total += 1
+#     end 
+#   end 
+#   total
+# end
